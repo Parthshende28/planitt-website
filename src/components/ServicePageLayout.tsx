5cd2c6045945ffc1a49d5e11e5181c7e038d8d31
Parth Mohit Shende <93974464+Parthshende28@users.noreply.github.com>
2025-11-10 20:32:37 +0530

    fixed double header and footer

--- src/components/ServicePageLayout.tsx
+++ src/components/ServicePageLayout.tsx
@@ -22,8 +22,6 @@ const ServicePageLayout = ({
 }: ServicePageLayoutProps) => {
     return (
         <div className="min-h-screen bg-white">
-            <Header />
-
             {/* Service Hero Section */}
             <section className="py-20 bg-gradient-to-br from-blue-50 via-white to-blue-100">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -55,8 +53,6 @@ const ServicePageLayout = ({
 
             {/* Contact Section */}
             <Contact />
-
-            <Footer />
         </div>
     );
 };
